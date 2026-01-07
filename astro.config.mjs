93a9a9fd146d6d39c88804dc6930ba8728be1ae5
Filip Juran <filipjuranoe@gmail.com>
2026-01-07 13:23:08 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import keystatic from "@keystatic/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "static",
+    integrations: [react(), tailwind({ applyBaseStyles: false }), keystatic()],
+});
